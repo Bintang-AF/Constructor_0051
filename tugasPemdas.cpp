decaf967e2e1e02346375692307c2c085fb58aed
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-05-17 18:51:25 +0700

    membuat class modifier

--- tugasPemdas.cpp
+++ tugasPemdas.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class dosen;
+class staff;
+class universitas;
+
+
+class mahasiswa{
+    private :
+    int nilai;
+
+    
+    public :
+    int nim;
+    string nama;
+
+    mahasiswa () {nim, nama, nilai;}
+    friend void dosen::tampilkannilaipelajar(mahasiswa &x);
+
+};
+
