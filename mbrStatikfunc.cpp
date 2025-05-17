9d08335be2bacdad8378a3dcff39151f809d9a50
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-05-17 15:32:09 +0700

    membuat medifier

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -2,3 +2,19 @@
 #include <string>
 using namespace std;
 
+class mahasiswa{
+    private :
+    static int nim;
+    public :
+    int id;
+    string nama;
+
+    void setid();
+    void printall();
+
+    static void setnim(int pnim){nim=pnim; /*definisi function*/}
+    static int getnim(){return nim;}
+
+    mahasiswa (string pnama):nama(pnama){setid();}
+
+};
