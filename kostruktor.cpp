6b2afb2d6ded0cf30264ccf45eaf510e6d1938ae
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-05-17 15:23:30 +0700

    membuat medifier serta fungsi construktornya

--- kostruktor.cpp
+++ kostruktor.cpp
@@ -2,3 +2,41 @@
 #include <string>
 using namespace std;
 
+class mahasiswa{
+    private :
+    int nim ;
+    string nama;
+    public :
+    mahasiswa ();
+    mahasiswa (int);
+    mahasiswa (string);
+    mahasiswa (int inim, string inama);
+
+    void cetak();
+
+
+};
+
+mahasiswa :: mahasiswa(){
+    nim = 0;
+    nama = "";
+
+}
+
+mahasiswa :: mahasiswa(int inim){
+    nim = inim;
+}
+
+mahasiswa :: mahasiswa (string inama){
+    nama = inama;
+}
+
+mahasiswa :: mahasiswa (int inim, string inama){
+    nim = inim;
+    nama = inama;
+}
+
+void mahasiswa::cetak(){
+    cout << endl << "nim =" <<nim<<endl;
+    cout << " nama = " << nama<<endl;
+}
