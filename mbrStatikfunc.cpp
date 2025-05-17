5e44ecfcb20eb1e6714c88a4e195a2d8e4b8d247
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-05-17 15:38:04 +0700

    manampilkan fungsi serta pengaksesan nim

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -18,3 +18,32 @@ class mahasiswa{
     mahasiswa (string pnama):nama(pnama){setid();}
 
 };
+
+int mahasiswa::nim=0;
+
+void mahasiswa::setid(){
+    id = ++nim;
+}
+
+void mahasiswa::printall(){
+    cout << "id  = "<<id <<endl;
+    cout << "nama  = "<<nama<<endl;
+    cout << endl;
+}
+
+int main(){
+    mahasiswa mhs1("sridadi");
+    mahasiswa mhs2("budi jatmiko");
+    mahasiswa::setnim(9);
+    mahasiswa mhs3("andi janu");
+    mahasiswa mhs4 ("joko wahono");
+
+    mhs1.printall();
+    mhs2.printall();
+    mhs3.printall();
+    mhs4.printall();
+
+    cout << "akses dari luar object  = "<<mahasiswa::getnim()<<endl;
+
+    return 0;
+}
